ebbaa91b0451b25bda97bf62f6f389f7cf788f70
David Jack Wange Olrik <david@olrik.dk>
2026-02-16 12:40:44 +0100

    fix: Make version extraction more robust

--- internal/core/version.go
+++ internal/core/version.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-//go:generate sh -c "git describe --tags --long --dirty='-devel' --match '[0-9]*.[0-9]*.[0-9]*' > version.txt"
+//go:generate sh -c "(git describe --tags --long --dirty='-devel' --match '[0-9]*.[0-9]*.[0-9]*' || echo '0.0.0') > version.txt"
 //go:embed version.txt
 var version string
 
